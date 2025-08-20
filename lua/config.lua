0bc1368a5a3d7ab3ee3521f0d9a350a8ccfb75b8
Tbrosnan12 <thomasbrosnan12@gmail.com>
2025-08-21 00:17:44 +0100

    auto-sync

--- lua/config.lua
+++ lua/config.lua
@@ -1,3 +1,18 @@
+-- Bootstrap lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Now set up plugins
 require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter",
@@ -21,31 +36,33 @@ require("lazy").setup({
       local cmp = require("cmp")
       local luasnip = require("luasnip")
 
-     cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+          -- Override <C-p> to launch TypstPreview
+          ["<C-p>"] = cmp.mapping(function(fallback)
+            vim.cmd("stopinsert")
+            pcall(vim.cmd, "TypstPreview")
+            vim.cmd("TypstPreviewFollowCursor")
+            vim.cmd("startinsert")
+          end, { "i" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+      })
     end,
   },
-  mapping = {
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-    -- Override <C-p> to launch TypstPreview
-    ["<C-p>"] = cmp.mapping(function(fallback)
-      vim.cmd("stopinsert")
-      pcall(vim.cmd, "TypstPreview")
-      vim.cmd("TypstPreviewFollowCursor")
-      vim.cmd("startinsert")
-    end, { "i" }),
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  },
-})    end,
-  },
   {
     "chomosuke/typst-preview.nvim",
     ft = "typst",
@@ -59,10 +76,11 @@ require("lazy").setup({
   },
 })
 
+-- Colorscheme
 vim.opt.termguicolors = true
 vim.cmd("colorscheme gruvbox")
 
--- Your LSP config and keymaps below unchanged
+-- LSP config
 local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 
@@ -82,11 +100,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local opts = { buffer = bufnr }
     vim.keymap.set("i", "<C-i>", function()
       -- Use <C-o>K trick to run hover without leaving insert mode
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>K", true, false, true), "n", false)
+      vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes("<C-o>K", true, false, true),
+        "n",
+        false
+      )
     end, opts)
   end,
 })
 
+-- Command to open PDF output
 vim.api.nvim_create_user_command("OpenPdf", function()
   local filepath = vim.api.nvim_buf_get_name(0)
   if filepath:match("%.typ$") then
@@ -95,6 +118,6 @@ vim.api.nvim_create_user_command("OpenPdf", function()
   end
 end, {})
 
+-- Keybind for PDF
 vim.keymap.set("i", "<C-CR>", "<Esc>:OpenPdf<CR>a", { noremap = true, silent = true })
-
 
